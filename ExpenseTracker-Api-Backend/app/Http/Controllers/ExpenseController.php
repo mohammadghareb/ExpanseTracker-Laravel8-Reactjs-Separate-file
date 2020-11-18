a7743841101ef8e9446c8eed42d54e33ce997e4e
mohammadghareb <mohammadghareb35@gmail.com>
2020-11-18 08:22:31 -0500

    Update-backend-with-jetstream

---
+++ ExpenseTracker-Api-Backend/app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers; 
+use App\Models\Expense;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class ExpenseController extends Controller
+{
+    
+    public function index()
+    {   
+         $user_id=Auth::id();
+         $expenses = Expense::where('user_id',$user_id)->get();
+        return response()->json($expenses);
+    }
+
+   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // $request->validate([
+        //     'name' => 'required',
+        //     'amount' => 'required',
+        //     'description' => 'required' 
+        // ]);
+        $expense =  Expense::create([
+            'user_id' => Auth::id(),
+            'name' => $request->input('name'),
+            'amount' => $request->input('amount'),
+            'description' => $request->input('description'),
+        ]);
+       // $expense = Expense::create($request->all());
+        return response()->json(['message'=> 'expense created', 
+        'expense' => $expense]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Expense  $expense
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Expense $expense)
+    {
+        return $expense;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Expense  $expense
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Expense  $expense
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Expense $expense)
+    {
+        $request->validate([
+            'name' => 'required',
+            'amount' => 'required',
+            'description' => 'required' 
+        ]);
+       $expense->update($request->all());
+        
+        return response()->json([
+            'message' => 'expense updated!',
+            'expense' => $expense
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Expense  $expense
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Expense $expense)
+    {
+
+
+        $expense->delete();
+        return response()->json([
+            'message' => 'expense deleted'
+        ]);
+        
+    }
+}
---
+++ ExpenseTracker-Api-Backend/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExpenseController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::middleware('auth:sanctum')->get('/list-expenses',[ExpenseController::class,'index']);
+Route::middleware('auth:sanctum')->post('/expenses',[ExpenseController::class,'index']);
+Route::middleware('auth:sanctum')->post('/store-expenses', [ExpenseController::class,'store']);
+Route::middleware('auth:sanctum')->get('/expenses/{expense}',[ExpenseController::class, 'show']);
+Route::middleware('auth:sanctum')->put('/expenses/{expense}', [ExpenseController::class,'update']);
+Route::middleware('auth:sanctum')->delete('/expenses/{expense}', [ExpenseController::class,'destroy']);
+
