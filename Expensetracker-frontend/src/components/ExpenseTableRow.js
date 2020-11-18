c1b037da841af0b8fc3c87c8378367340906f073
mohammadghareb <mohammadghareb35@gmail.com>
2020-11-18 08:27:30 -0500

    Update-backend-with-jetstream

---
+++ Expensetracker-frontend/src/components/ExpenseTableRow.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
+
+export default class ExpenseTableRow extends Component {
+    constructor(props) {
+        super(props);
+        this.deleteExpense = this.deleteExpense.bind(this);
+    }
+
+    deleteExpense() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete('/api/expenses/' + this.props.obj.id)
+                .then((res) => {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )   
+                }).catch((error) => {
+                    console.log(error)
+                })
+             
+            }
+          })
+        
+    }
+    render() {
+        return (
+            <tr>
+                <td>{this.props.obj.name}</td>
+                <td>{this.props.obj.amount}</td>
+                <td>{this.props.obj.description}</td>
+                <td>
+                    <Link className="edit-link" to={"/edit-expense/" + this.props.obj.id}>
+                       <Button size="sm" variant="info">Edit</Button>
+                    </Link>
+                    <Button onClick={this.deleteExpense} size="sm" variant="danger">Delete</Button>
+                </td>
+            </tr>
+        );
+    }
+}
